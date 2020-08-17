7161c0099b67bdb41ba51d7ca058c662a6b48794
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-08-17 19:15:56 +0200

    Added record button, change styling of the piano

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
-document.addEventListener("keypress", function (event: any) {
+document.addEventListener("keypress", function (event: KeyboardEvent) {
 
     let audio = document.createElement("AUDIO") as HTMLAudioElement;
 
     if(event.key === "a") {
         audio.src = "./white_keys/A.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "s") {
         audio.src = "./white_keys/S.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "d") {
         audio.src = "./white_keys/D.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "f") {
         audio.src = "./white_keys/F.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "g") {
         audio.src = "./white_keys/G.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "h") {
         audio.src = "./white_keys/H.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "j") {
         audio.src = "./white_keys/J.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "w") {
         audio.src = "./black_keys/W.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "e") {
         audio.src = "./black_keys/E.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "t") {
         audio.src = "./black_keys/T.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "y") {
         audio.src = "./black_keys/Y.mp3";
-        audio.play().then();
+        audio.play();
     } else if (event.key === "u") {
         audio.src = "./black_keys/U.mp3";
-        audio.play().then();
+        audio.play();
     }
 
 })
